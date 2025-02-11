77b68aca038001a0312a1f483f85ff04017d2684
KHAN IMRAN <imrankhanshakeel012@gmail.com>
2025-02-11 19:22:12 +0530

    configure redux into app and also configure tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import conf from "./conf/conf";
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth"
+import { login, logout } from "./feature/authSlice"
+import { Header, Footer } from "./components"
+// import Outlet from "react-router-dom"
 
 function App() {
-  console.log(conf);
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          Main
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
